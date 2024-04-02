f3d64e5be09b9b8aaab27d7a5de1eb529ddcb8a5
Ivan Koryukov <koryukov@greensight.ru>
2024-04-02 12:22:51 +0300

    DEVOPS-1217 скрыт токен докерхаба из логов, добавлена функция Helm.findValue для поиска значений в values файлах

--- vars/imageExistsInRegistry.groovy
+++ vars/imageExistsInRegistry.groovy
@@ -5,15 +5,17 @@ def call(registryCredsId, registryAddress, imageName, imageTag) {
     try {
         withCredentials([usernamePassword(credentialsId: registryCredsId, usernameVariable: 'username', passwordVariable: 'password')]) {
             def harborToken = sh(returnStdout:true, script: """
+                                set +x
                                 curl -s -k \
                                     -u '${username}:${password}' \
                                     '${registryAddress}/service/token?service=harbor-registry&scope=repository:${imageName}:pull' | \
-                                    python -c 'import json,sys; obj = json.load(sys.stdin); print(obj["token"]);'
+                                    python -c 'import json,sys; obj = json.load(sys.stdin); print(obj["token"]);' > harbor-token.txt
                             """)
             def statusCode = sh(returnStdout:true, script: """
+                                set +x
                                 curl -s -k \
                                     -H 'Content-Type: application/json' \
-                                    -H "Authorization:  Bearer ${harborToken}" \
+                                    -H "Authorization:  Bearer \$(cat harbor-token.txt)" \
                                     -X GET \
                                     -o /dev/null -w '%{http_code}' \
                                     '${registryAddress}/v2/${imageName}/manifests/${imageTag}'
