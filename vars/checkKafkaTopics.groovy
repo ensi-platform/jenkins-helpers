758f80fc512daa9342800dbf83c2c6dd2b71b03f
C0rTeZ13 <work.cortez13@gmail.com>
2025-03-21 11:00:12 +0300

    ensitech-273 remove file creation

--- vars/checkKafkaTopics.groovy
+++ vars/checkKafkaTopics.groovy
@@ -4,14 +4,11 @@ def call(appImage, connectionParams, missedTopicsFile) {
     docker.image(appImage).inside('--entrypoint=""') {
         sh(script: """
         ${connectionParams}
-        if [ -n "${missedTopicsFile}" ] && [ ! -f "${missedTopicsFile}" ]; then
-                    touch "${missedTopicsFile}"
-        fi
         cmd=\$(php /var/www/artisan list | grep 'kafka:find-not-created-topics')
         if [ "\${cmd}" != "" ]; then
             if [ -n "${missedTopicsFile}" ]; then
-                php /var/www/artisan kafka:find-not-created-topics --file="${missedTopicsFile}"
-                cat "${missedTopicsFile}"
+                php /var/www/artisan kafka:find-not-created-topics --file=${missedTopicsFile}
+                cat ${missedTopicsFile}
             else
                 php /var/www/artisan kafka:find-not-created-topics
             fi
