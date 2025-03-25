5fa757a7c4966475cec4671c21ca67f0bf29d02b
C0rTeZ13 <work.cortez13@gmail.com>
2025-03-25 19:39:38 +0300

    ensitech-273 update

--- vars/checkKafkaTopics.groovy
+++ vars/checkKafkaTopics.groovy
@@ -6,7 +6,10 @@ def call(appImage, connectionParams, missedTopicsFile) {
         ${connectionParams}
         cmd=\$(php /var/www/artisan list | grep 'kafka:find-not-created-topics')
         if [ "\${cmd}" != "" ]; then
-            php /var/www/artisan kafka:find-not-created-topics --file=${missedTopicsFile}
+            if [ ! -f "${missedTopicsFile}" ]; then
+                touch "${missedTopicsFile}"
+            fi
+            php -d error_reporting="E_ALL & ~E_DEPRECATED" /var/www/artisan kafka:find-not-created-topics --file=${missedTopicsFile} 2>/dev/null
             cat ${missedTopicsFile}
         else
             echo 'Service does not have command kafka:find-not-created-topics'
