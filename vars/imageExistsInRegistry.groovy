37b03c51db52dabd46ebb05f48922dd635cc40b1
Ivan Koryukov <koryukov@greensight.ru>
2025-08-04 18:15:05 +0300

    DEVOPS-3016 change python to jq in imageExistsInRegistry step

--- vars/imageExistsInRegistry.groovy
+++ vars/imageExistsInRegistry.groovy
@@ -4,22 +4,19 @@ def call(registryCredsId, registryAddress, imageName, imageTag) {
     def result = false
     try {
         withCredentials([usernamePassword(credentialsId: registryCredsId, usernameVariable: 'username', passwordVariable: 'password')]) {
-            def harborToken = sh(returnStdout:true, script: """
-                                set +x
-                                curl -s -k \
-                                    -u '${username}:${password}' \
-                                    '${registryAddress}/service/token?service=harbor-registry&scope=repository:${imageName}:pull' | \
-                                    python -c 'import json,sys; obj = json.load(sys.stdin); print(obj["token"]);' > harbor-token.txt
-                            """)
             def statusCode = sh(returnStdout:true, script: """
-                                set +x
-                                curl -s -k \
-                                    -H 'Content-Type: application/json' \
-                                    -H "Authorization:  Bearer \$(cat harbor-token.txt)" \
-                                    -X GET \
-                                    -o /dev/null -w '%{http_code}' \
-                                    '${registryAddress}/v2/${imageName}/manifests/${imageTag}'
-                            """).trim()
+                set +x
+                curl -s -k \
+                    -u '${username}:${password}' \
+                    '${registryAddress}/service/token?service=harbor-registry&scope=repository:${imageName}:pull' | \
+                    jq -r '.token' > harbor-token.txt
+                curl -s -k \
+                    -H 'Content-Type: application/json' \
+                    -H "Authorization:  Bearer \$(cat harbor-token.txt)" \
+                    -X GET \
+                    -o /dev/null -w '%{http_code}' \
+                    '${registryAddress}/v2/${imageName}/manifests/${imageTag}'
+            """).trim()
 
             result = ("200" == statusCode)
         }
