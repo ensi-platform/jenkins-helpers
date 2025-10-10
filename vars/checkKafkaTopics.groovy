f1a1ec86ce06a29dcc56224f67e49e0113c29b98
n1k15 <84707424+n1k15@users.noreply.github.com>
2025-10-10 13:57:30 +0300

    devops-3302 fix2

--- vars/checkKafkaTopics.groovy
+++ vars/checkKafkaTopics.groovy
@@ -6,7 +6,7 @@ def call(appImage, connectionParams, missedTopicsFile) {
         ${connectionParams}
         cmd=\$(php /var/www/artisan list | grep 'kafka:find-not-created-topics')
         if [ "\${cmd}" != "" ]; then
-            if php /var/www/artisan help kafka:find-not-created-topics 2>/dev/null | grep -q "\-\-file"; then
+            if php /var/www/artisan help kafka:find-not-created-topics 2>/dev/null | grep -q -- '--file'; then
                 php /var/www/artisan kafka:find-not-created-topics --file=${missedTopicsFile}
             else
                 php /var/www/artisan kafka:find-not-created-topics > ${missedTopicsFile}
